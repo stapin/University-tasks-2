c54cedf73445b04ec98340402fb1383df9649cc5
stapin <76260508+stapin@users.noreply.github.com>
2022-02-24 19:43:10 +0300

    + merge(), fix poly_get()

--- polynom.c
+++ polynom.c
@@ -8,111 +8,115 @@ typedef struct poly
     struct poly *next;
 } poly;
 
-// poly *poly_get(const char *str)
-// {
-//     poly *result = malloc(sizeof(poly));
-//     poly *curr = result;
-//     char *p = str;
-//     int sign = 1;
-//     int state = 1;
-//     do
-//     {
-//         // добавить проверки на конец строки.
-//         if (*p == ' ')
-//         {
-//             p++;
-//             continue;
-//         }
-//         if (state == 1)
-//         {
-//             curr->coeff = strtol(p, &p, 0) * sign;
-//             ++state;
-//         }
-//         if (state == 2)
-//         {
-//             if (*p == 'x')
-//             {
-//                 ++p;
-//                 ++state;
-//             }
-//             else
-//             {
-//                 printf("%s\n", "Error state 2!");
-//                 return NULL;
-//             }
-//         }
-//         if (state == 3)
-//         {
-//             if (*p == '^')
-//             {
-//                 ++p;
-//                 ++state;
-//             }
-//             else
-//             {
-//                 printf("%s\n", "Error state 3!");
-//                 return NULL;
-//             }
-//         }
-//         if (state == 4)
-//         {
-//             curr->exp = strtol(p, &p, 0);
-//             ++state;
-//         }
-//         while (*p == ' ') ++p;
-//         if (*p == '-' || *p == '+')
-//         {
-//             curr->next = malloc(sizeof(poly));
-//             curr = curr->next;
-//             sign = 44 - *p;
-//             state = 1;
-//             ++p;
-//         }
-//     }
-//     while (*p);
-//     curr->next = NULL;
-//     return result;
-// }
 
 
-// 3й вариант(простой).
+void poly_free(poly *head)
+{
+    if (!head) 
+    {
+        puts("Warning: try to free NULL. [poly_free]");
+        return;
+    }
+    poly *curr = head;
+    poly *next;
+    while (curr->next)
+    {
+        next = curr->next;
+        free(curr);
+        curr = next;   
+    }
+    return;
+}
+
+
+
+// Добавить функцию merge(poly*, int coeff, int exp);
+// Добавляет в отсортированный список мономов.
+
+int merge(poly **head, int coeff, int exp)
+{
+    if (!head) return 0;
+    if (!(*head))
+    {
+        *head = malloc(sizeof(poly));
+        (*head)->coeff = coeff;
+        (*head)->exp = exp;
+        (*head)->next = NULL;
+        return 1;
+    }
+    
+    if ((*head)->exp > exp)
+    {
+        poly *tmp = *head;
+        *head = malloc(sizeof(poly));
+        (*head)->coeff = coeff;
+        (*head)->exp = exp;
+        (*head)->next = tmp;
+        return 1;
+    }
+    
+
+    poly *curr = *head;
+    while (curr->next)
+    {
+        if (curr->next->exp <= exp) curr = curr->next; // => (curr->exp <= exp)
+        // (next->exp > exp) для остальных условий.
+        else if (curr->exp == exp)
+        {
+            curr->coeff += coeff;
+            return 1;
+        }
+        else if (curr->exp < exp)
+        {
+            poly *tmp = curr->next;
+            curr->next = malloc(sizeof(poly));
+            curr->next->coeff = coeff;
+            curr->next->exp = exp;
+            curr->next->next = tmp;
+            return 1;
+        }
+    }
+    // curr->exp <= exp
+    if (curr->exp == exp)
+    {
+        curr->coeff = coeff;
+        return 1;
+    }
+    curr->next = malloc(sizeof(poly));
+    curr = curr->next;
+    curr->coeff = coeff;
+    curr->exp = exp;
+    curr->next = NULL;
+    return 1;
+}
+
+// version 2.
+// add macros for states.
 poly *poly_get(const char *str)
 {
-    poly *result = malloc(sizeof(poly));
-    poly *curr = result;
+    poly *result = NULL;
+    // poly *curr = result;
     const char *p = str;
+    int curr_coeff = 0;
+    int curr_exp = 0;
     int sign = 1;
     int state = 1;
-    do
+    char *end = NULL;
+    while (*p)
     {
-        if (!(*p))
-        {
-            if (state != 5)
-            {
-                printf("%s\n", "Error, expected state 5. Wrong input.");
-                return NULL;
-            }
-            else break;
-        }
-        
         if (*p == ' ')
         {
-            p++;
+            ++p;
             continue;
         }
         if (state == 1)
         {
-            char **end = &p;
-            curr->coeff = strtol(p, end, 0) * sign;
-            // if (!end)
-            // {
-            //     printf("end == NULL.");
-            //     return NULL;
-            // }
-            p = *end;
-            if (!curr->coeff)
+            curr_coeff = strtol(p, &end, 0) * sign;
+            p = end;
+            if (!curr_coeff)
             {
                 printf("%s\n", "Wrong input. State 1, expected number.");
+                poly_free(result);
                 return NULL;
             }
             ++state;
@@ -124,13 +128,16 @@ poly *poly_get(const char *str)
             {
                 ++p;
                 ++state;
+                continue;
             }
             else
             {
-                printf("%s\n", "Error state 2!");
-                return NULL;
+                // printf("%s\n", "Error state 2!");
+                // return NULL;
+                curr_exp = 0;
+                merge(&result, curr_coeff, curr_exp);
+                state = 5;
             }
-            continue;
         }
         if (state == 3)
         {
@@ -141,55 +148,72 @@ poly *poly_get(const char *str)
             }
             else
             {
-                printf("%s\n", "Error state 3!");
-                return NULL;
+                curr_exp = 1;
+                merge(&result, curr_coeff, curr_exp);
+                state = 5;
+                // printf("%s\n", "Error state 3!");
+                // return NULL;
             }
             continue;
         }
         if (state == 4)
         {
-            curr->exp = strtol(p, &p, 0);
-            if (!curr->exp)
+
+            curr_exp = strtol(p, &end, 0);
+            p = end;
+            if (!curr_exp)
             {
                 printf("%s\n", "Wrong input. State 4, expected number.");
+                poly_free(result);
                 return NULL;
             }
+            merge(&result, curr_coeff, curr_exp);
             ++state;
             continue;
         }
-        if ((state == 5) && (*p == '-' || *p == '+'))
+        if ((state == 5))
         {
-            curr->next = malloc(sizeof(poly));
-            curr = curr->next;
-            sign = 44 - *p;
-            state = 1;
-            ++p;
-            continue;
+            if (*p == '-' || *p == '+')
+            {
+                sign = 44 - *p;
+                state = 1;
+                ++p;
+                continue;
+            }
+            puts("Wrong input, state 5. Exepted sign.");
+            poly_free(result);
+            return NULL;
         }
         else
         {
             puts("Unknown error.");
+            poly_free(result);
+            return NULL;
         }
     }
-    while (1);
 
-    curr->next = NULL;
+    if (state != 5 && state != 1)
+    {
+        printf("%s\n", "Error, expected state 5. Wrong input.");
+        return NULL;
+    }
     
     return result;
 }
 
-poly *poly_add(poly *a, poly *b)
+
+poly *poly_add(const poly *a, const poly *b)
 {
     if (!a || !b)
     {
-        puts("Add error!");
+        puts("poly_add error! arguments are NULL.");
         return NULL;
     }
     
     poly *result = malloc(sizeof(poly));
     poly *curr = result;
-    poly *p1 = a;
-    poly *p2 = b;
+    const poly *p1 = a;
+    const poly *p2 = b;
     do
     {
         if (p1->exp < p2->exp)
@@ -267,52 +291,35 @@ void poly_print(const poly *head)
     const poly *curr = head;
     while (curr->next)
     {
-        printf("%dx^%d + ", curr->coeff, curr->exp);
+        if (!curr->exp) printf("%d + ", curr->coeff);
+        else if (curr->exp == 1) printf("%dx + ", curr->coeff);
+        else printf("%dx^%d + ", curr->coeff, curr->exp);
         curr = curr->next;
     }
-    printf("%dx^%d\n", curr->coeff, curr->exp);
+    if (!curr->coeff) printf("%d\n", curr->coeff);
+    else if (curr->coeff == 1) printf("%dx\n", curr->coeff);
+    else  printf("%dx^%d\n", curr->coeff, curr->exp);
 }
 
-void poly_free(poly *head)
+
+void test_add()
 {
-    poly *curr = head;
-    poly *next;
-    while (curr->next)
-    {
-        next = curr->next;
-        free(curr);
-        curr = next;   
-    }
-    return;
-}
-
-int main()
-{
-    poly *head = malloc(sizeof(poly));
-    poly *curr = head;
-    for (size_t i = 0; i < 10; i++)
-    {
-        curr->coeff = 1;
-        curr->exp = 1;
-        curr->next = malloc(sizeof(poly));
-        curr = curr->next;
-    }
-    curr->coeff = 1;
-    curr->exp = 1;
-    curr->next = NULL;
-
-    poly_print(head);
-
-     
     //char *p1 = "4x^2 + 5x^4 - 159x^8";
-    char *p2 = "25x^1 - -14x^4 + 2x^100";
+    char *p2 = "25x^1 - -14x^4 + 4x^1 --3 + 2x^100";
     poly *a = poly_get(p2);
-    poly *b = poly_get("3x^2 - 1x^3 - 8x^4 + 158x^8 - 6x^13");
+    poly *b = poly_get("3x^2 - 1x^3 - 8x^4 + 158x^8 + 8 - 6x^13");
     poly_print(a);
     poly_print(b);
     poly *c = poly_add(a, b);
     poly_print(c);
     poly_free(c);
+    poly_free(a);
+    poly_free(b);
 
+}
+
+int main()
+{    
+    test_add();
     return 0;
 }
